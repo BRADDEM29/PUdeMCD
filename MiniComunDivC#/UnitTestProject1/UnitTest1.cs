57b2f1c89d84c617b4fb07fcd17038a633b4dd58
BRADDEM29 <72587556@continental.edu.pe>
2024-05-04 21:41:28 -0500

    Agregar archivos de proyecto.

---
+++ MiniComunDivC#/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Proyecto;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+    }
+}
